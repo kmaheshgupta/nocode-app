74ba763a6703345fad228aed193f6e8976e1fa16
Mahesh Kesarla <mahesh.kesarla@wabtec.com>
2024-12-22 23:50:17 +0530

    Added employee charts

---
+++ theme.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const theme = localStorage.getItem("theme") || "dark-mode";
+  document.body.classList.add(theme);
+  if (theme === "light-mode") {
+    document.getElementById("theme-toggle").checked = true;
+  }
+  applyThemeToElements(theme);
+});
+
+function toggleMode() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
+  const theme = body.classList.contains("dark-mode")
+    ? "dark-mode"
+    : "light-mode";
+  localStorage.setItem("theme", theme);
+  applyThemeToElements(theme);
+}
+
+function applyThemeToElements(theme) {
+  const elements = document.querySelectorAll(".visualization-box");
+  elements.forEach((element) => {
+    if (theme === "dark-mode") {
+      element.style.backgroundColor = "#222";
+      element.style.borderColor = "#555";
+    } else {
+      element.style.backgroundColor = "#f9f9f9";
+      element.style.borderColor = "#ccc";
+    }
+  });
+}
